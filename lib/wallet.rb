5910901aba072bc8f9dc45b4ebc93cbb13ac2966
Colin Osborn <colin.osborn@gmail.com>
2016-04-08 13:13:06 -0600

    fixing some typos

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -19,10 +19,10 @@ end
 # keys?
 # prints the public
 
-To start with a basic wallet implementation,
-write a program which, when run, will look for a special set of wallet files on the user's computer. We'll use a
-common format for serializing RSA keys called .pem,
-so let's assume these files are
-~/.wallet/public_key.pem and
- ~/.wallet/private_key.pem,
- respectively.
+# To start with a basic wallet implementation,
+# write a program which, when run, will look for a special set of wallet files on the user's computer. We'll use a
+# common format for serializing RSA keys called .pem,
+# so let's assume these files are
+# ~/.wallet/public_key.pem and
+#  ~/.wallet/private_key.pem,
+#  respectively.
