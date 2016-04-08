d2f5ce8c9f919fdce7dcea50fa7685852bda54f6
Colin Osborn <colin.osborn@gmail.com>
2016-04-08 14:50:08 -0600

    tests baby

--- test/keygen_test.rb
+++ test/keygen_test.rb
@@ -1,16 +1,20 @@
-require 'pry'
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'base64'
-require './lib/keygen'
-
+require_relative './test_helper'
+require_relative '../lib/keygen.rb'
 
 class KeyGenTest < Minitest::Test
 
-  def test_we_can_return_public_key
-    keygen = KeyGen.new
-    private_key = keygen.private_key
-    assert nil, private_key
+  def test_can_generate_private_key
+    keygen      = KeyGen.new
+    private_key = keygen.private_key_pem
+
+    assert private_key.include?("BEGIN RSA PRIVATE KEY")
+    assert private_key.include?("END RSA PRIVATE KEY")
   end
 
-end
+  def test_can_generate_public_key
+    keygen      = KeyGen.new
+    public_key = keygen.public_key_pem
+
+    assert public_key.include?("BEGIN PUBLIC KEY")
+    assert public_key.include?("END PUBLIC KEY")
+  end
